a527dae2a9b5d109eeb2b31c9ebad09bd05eb817
FrankT <306933@gmail.com>
2019-03-12 12:01:42 -0700

    added to test
    messing around more in test

--- src/Pack1/Test.java
+++ src/Pack1/Test.java
@@ -3,7 +3,12 @@ package pack1;
 import acm.graphics.GLabel;
 import acm.program.*;
 import acm.graphics.*;
+
+import java.awt.Color;
 import java.util.*;
+
+// https://www.youtube.com/watch?v=oW2apwWxetI
+// learning from this vid
 
 public class Test extends GraphicsProgram  {
 	
@@ -16,6 +21,20 @@ public class Test extends GraphicsProgram  {
 		
 		canvas = new GCanvas();
 		this.add(canvas);
+		
+		this.drawHouse();
+		//this.drawSun();
+		//this.drawTree();
+		
+		canvas.revalidate();
+	}
+	
+	void drawHouse() {
+		// Colors for the house
+		Color lightBlue = new Color(183, 234, 237);
+		
+		
+		
 	}
 	
 	
