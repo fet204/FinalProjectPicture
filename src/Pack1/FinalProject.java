6f8858632ba71051969e6379dd7471b6d289a0bb
FrankT <306933@gmail.com>
2019-03-12 11:29:56 -0700

    imported acm
    Testing and learning GPen

--- src/Pack1/FinalProject.java
+++ src/Pack1/FinalProject.java
@@ -1,5 +1,30 @@
+/*
+ * File: FinalProject.java
+ * Function: This is going to be the picture for my final project. 
+ */
 package Pack1;
 
-public class FinalProject {
+import acm.program.*;
+import acm.graphics.*;
+
+public class FinalProject extends GraphicsProgram {
+	
+	public void run() {
+		
+		GLabel label1 = new GLabel("Hello world");
+		add(label1, 100, 75);
+		
+		while(true) {
+			label1.move(1, 1);
+			pause(100);
+		}
+			
+	}
+	
+	
+	public static void main (String[] args) {
+		
+		
+	}
 
 }
