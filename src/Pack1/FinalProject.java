90cd74915844a0626cf31527f02ba8e2be2788ab
FrankT <306933@gmail.com>
2019-03-12 11:58:09 -0700

    Moving code from final to test
    Moving the actual test code to the actual test java class

--- src/Pack1/FinalProject.java
+++ src/Pack1/FinalProject.java
@@ -2,29 +2,21 @@
  * File: FinalProject.java
  * Function: This is going to be the picture for my final project. 
  */
-package Pack1;
+package pack1;
 
 import acm.program.*;
 import acm.graphics.*;
+import java.util.*;
 
 public class FinalProject extends GraphicsProgram {
 	
 	public void run() {
 		
-		GLabel label1 = new GLabel("Hello world");
-		add(label1, 100, 75);
 		
-		while(true) {
-			label1.move(1, 1);
-			pause(100);
-		}
 			
-	}
+	} // run
 	
 	
-	public static void main (String[] args) {
-		
-		
-	}
 
-}
+
+} // FinalProject
--- src/Pack1/Test.java
+++ src/Pack1/Test.java
@@ -1,6 +1,38 @@
-package Pack1;
+package pack1;
 
-public class Test {
-	// test
+import acm.graphics.GLabel;
+import acm.program.*;
+import acm.graphics.*;
+import java.util.*;
 
-}
+public class Test extends GraphicsProgram  {
+	
+	GCanvas canvas;
+	
+	void setup() {
+		this.setSize(500, 500);
+		this.setVisible(true);
+		this.setTitle("My Picture"); 
+		
+		canvas = new GCanvas();
+		this.add(canvas);
+	}
+	
+	
+	
+	
+	
+	public void run() {
+		GLabel label1 = new GLabel("Hello world");
+		add(label1, 100, 75);
+		//GPen testPen(5,5);
+		
+		for (int i = 0; i < 20; i++) {
+			label1.move(1, 1);
+			pause(100);
+	
+		} // the while loop
+		
+	} // run
+	
+}//Test class
