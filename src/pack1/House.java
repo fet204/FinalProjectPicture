e30bed033b36016ce57d0ffc20f34c354deec163
FrankT <306933@gmail.com>
2019-03-13 14:00:18 -0700

    made body of house
    Making body of the house

--- src/pack1/House.java
+++ src/pack1/House.java
@@ -1,5 +1,98 @@
 package pack1;
 
-public class House {
+import acm.program.GraphicsProgram;
+import acm.graphics.GLabel;
+import acm.graphics.GOval;
+import acm.graphics.GPen;
+import acm.graphics.*;
+import java.awt.*;
+import acm.graphics.GPolygon;
+import java.awt.Color;
 
-}
+/* websites
+ * this is the color wheel to get sick RGB colors
+ * https://color.adobe.com/create/color-wheel/
+ */
+
+
+
+
+// BASIC HOUSE ART	
+public class House extends GraphicsProgram {
+	
+	// Size set of the art
+	public static final int height = 500; 
+	public static final int width = 1000;
+	
+	public void init() {
+		
+		setSize(width, height);
+		
+	} // init
+	
+	public void run() {
+		
+		// Start coordinate of where GPen will be and start
+		GPen pen = new GPen(500, 100);
+		add(pen);
+		
+		//1 means code will instantly show art, and 0 would be the slowest 
+		pen.setSpeed(1);
+		
+		// Outline of house
+		pen.setColor(Color.BLACK);
+		
+		// Fill in house
+		pen.setFillColor(new Color(97, 255, 73));
+		
+		//Where rectangle will be coordinated in the appletViewer
+		pen.startFilledRegion();
+		
+		// Dimensions of Rectangle
+		pen.drawLine(200, 0); // width
+		pen.drawLine(0, 200); // Height
+		pen.drawLine(-200, 0); // -Width
+		pen.drawLine(0, -200); // -Height
+		
+		pen.endFilledRegion();
+		
+		// Draw Roof by connecting vertices 
+		GPolygon roof = new GPolygon();
+		roof.addVertex(500, 100);
+		roof.addVertex(300, 100);
+		roof.addVertex(400, 0);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}// run
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // Graphics program
