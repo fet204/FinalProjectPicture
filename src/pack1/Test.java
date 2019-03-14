ff6f4b05d164a3c73c93089c75026dd1910399b6
FrankT <306933@gmail.com>
2019-03-14 16:29:42 -0700

    fixing gradient
    Just trying to get the gradient together.

--- src/pack1/Test.java
+++ src/pack1/Test.java
@@ -1,6 +1,11 @@
 /*
  * File: FinalProject.java
  * Function: This is going to be the picture for my final project. 
+ * 
+ * WEBSITES THAT HELP 
+ * https://imagecolorpicker.com/
+ * https://color.adobe.com/create/color-wheel/
+ * 
  */
 package pack1;
 
@@ -10,8 +15,8 @@ import java.awt.*;
 
 public class Test extends GraphicsProgram {
 	
-	public static final int height = 500; 
-	public static final int width = 1000;
+	public static final int height = 500; // originally 500
+	public static final int width = 1500; // originally 1000
 	
 	public void init() {
 		
@@ -22,17 +27,17 @@ public class Test extends GraphicsProgram {
 	public void run() {
 		
 		// Start coordinate of where GPen will be and start
-		GPen pen = new GPen(500, 100);
+		GPen pen = new GPen(500, 100); // orignially x is 500 and y is 100
 		add(pen);
 		
 		//1 means code will instantly show art, and 0 would be the slowest 
 		pen.setSpeed(1); //0.7);
 		
 		// Outline of house
-		pen.setColor(Color.BLACK);
+		//pen.setColor(Color.BLACK);
 		
 		// Fill in house
-		pen.setFillColor(new Color(97, 255, 73));
+		//pen.setFillColor(new Color(97, 255, 73));
 		
 		//Where rectangle will be coordinated in the appletViewer
 		//pen.startFilledRegion();
@@ -40,9 +45,77 @@ public class Test extends GraphicsProgram {
 		// The Sky
 		Color sky1 = new Color(137, 164, 227);
 		pen.setColor(sky1);
-		pen.move(-511, -100);
-		pen.move(0,500);
-		double x = .1;
+		pen.move(-500, -100);
+		pen.drawLine(1000,0);
+		for (int i = 2; i <= 500; i ++) {
+			pen.move(0, 1);
+			if(i == 25) {
+				sky1 = new Color (144, 185, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 50) {
+				sky1 = new Color (145, 189, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 75) {
+				sky1 = new Color (147, 193, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 100) {
+				sky1 = new Color (148, 197, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 125) {
+				sky1 = new Color (150, 201, 226);
+				pen.setColor(sky1);
+			}			
+			if(i == 150) {
+				sky1 = new Color (151, 205, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 175) {
+				sky1 = new Color (153, 209, 226);
+				pen.setColor(sky1);
+			}		
+			if(i == 200) {
+				sky1 = new Color (154, 213, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 225) {
+				sky1 = new Color (156, 217, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 250) {
+				sky1 = new Color (157, 221, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 275) {
+				sky1 = new Color (159, 226, 226);
+				pen.setColor(sky1);
+			}
+			if(i == 300) {
+				sky1 = new Color (162, 226, 226);
+				pen.setColor(sky1);
+			}
+			if (i == 301) {
+				sky1 = new Color(166, 226, 226);
+				pen.setColor(sky1);
+			}
+			
+			
+			
+			
+			if (i % 2 == 0) {
+				pen.drawLine(-1000, 0);
+			}
+			else
+				pen.drawLine(1000, 0);	
+		}// first block sky21
+		
+		Color sky2 = new Color(137, 164, 227);
+		pen.setColor(sky2);
+		pen.move(-5, 83);
+		//pen.move(0,500);
 		double y = 0;
 		for (int i = 2; i <= 284; i ++) {
 			
@@ -54,8 +127,30 @@ public class Test extends GraphicsProgram {
 			pen.drawLine(y, 0);
 			pen.move(-y, -1);
 			
-		}// first block sky21
+		}// first block for the side and bottom
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		// Dimensions of Rectangle
+		pen.drawLine(200, 0); // width
+		pen.drawLine(0, 200); // Height
+		pen.drawLine(-200, 0); // -Width
+		pen.drawLine(0, -200); // -Height
+		
+		pen.endFilledRegion();
+		
+		*/
+	
 	}
-}
+
+} // FinalProject
